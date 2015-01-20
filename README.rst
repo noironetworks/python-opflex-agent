f23b3e27761b8eaf68848ca92a1268af52ba197a
Ivar Lazzaro <ivarlazzaro@gmail.com>
2015-01-19 17:27:26 -0800

    Python opflex agent repo init

---
+++ README.rst
@@ -0,0 +1,22 @@
+=======
+Apicapi
+=======
+
+Provides edge policy enforcement.
+
+Install
+=======
+
+Using pip::
+
+    pip install python-opflex-agent
+
+easy_install::
+
+    easty_install python-opflex-agent
+
+manually::
+
+    git clone https://github.com/noironetworks/python-opflex-agent.git
+    cd python-opflex-agent
+    python setup.py install
---
+++ setup.py
@@ -0,0 +1,35 @@
+# Copyright (c) 2014 Cisco Systems
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+# @author: Ivar Lazzaro (ivar-lazzaro), Cisco Systems Inc.
+
+import setuptools
+
+
+setuptools.setup(
+    name="python-opflex-agent",
+    version="0.1.0",
+    packages=setuptools.find_packages(exclude=["*.tests", "*.tests.*",
+                                               "tests.*", "tests"]),
+    author="Cisco Systems, Inc.",
+    author_email="apicapi@noironetworks.com",
+    url="http://github.com/noironetworks/python-opflex-agent/",
+    license="http://www.apache.org/licenses/LICENSE-2.0",
+    description="This neutron agent provides edge policy enforcement.",
+    entry_points={
+        'console_scripts': [
+            'opflex-ovs-agent = opflexagent.gbp_ovs_agent:main']
+    }
+)
