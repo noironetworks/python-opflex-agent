9cbe10d641d502cab8af451183fbf4568bec2d01
Ratnakar Kolli <ratnakar.kolli@bigswitch.com>
2015-11-24 10:28:48 -0800

    Update setup.py
    Bump version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ import setuptools
 
 setuptools.setup(
     name="neutron-opflex-agent",
-    version="2014.2.1",
+    version="2014.2.2",
     packages=setuptools.find_packages(exclude=["*.tests", "*.tests.*",
                                                "tests.*", "tests"]),
     author="Cisco Systems, Inc.",
