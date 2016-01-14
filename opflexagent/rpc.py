1775b1f7151de65db9ab71e49ee1a76c16b4b217
Ivar Lazzaro <ivarlazzaro@gmail.com>
2016-01-13 19:39:20 -0800

    undo new RPC calls definition at this time of the refactor

--- opflexagent/rpc.py
+++ opflexagent/rpc.py
@@ -1,3 +1,4 @@
+
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
 #    a copy of the License at
@@ -19,17 +20,11 @@ import oslo_messaging
 LOG = logging.getLogger(__name__)
 
 TOPIC_OPFLEX = 'opflex'
-ENDPOINT = 'endpoint'
-VRF = 'vrf'
 
 
 class AgentNotifierApi(object):
-    """Server side notification API:
 
-    - Version 1.2: add opflex update
-    """
-
-    BASE_RPC_API_VERSION = '1.2'
+    BASE_RPC_API_VERSION = '1.1'
 
     def __init__(self, topic):
         target = oslo_messaging.Target(
@@ -39,10 +34,6 @@ class AgentNotifierApi(object):
                                                        topics.UPDATE)
         self.topic_subnet_update = topics.get_topic_name(topic, topics.SUBNET,
                                                          topics.UPDATE)
-        self.topic_opflex_endpoint_update = topics.get_topic_name(
-            topic, TOPIC_OPFLEX, ENDPOINT, topics.UPDATE)
-        self.topic_opflex_vrf_update = topics.get_topic_name(
-            topic, TOPIC_OPFLEX, VRF, topics.UPDATE)
 
     def port_update(self, context, port):
         cctxt = self.client.prepare(fanout=True, topic=self.topic_port_update)
@@ -53,23 +44,9 @@ class AgentNotifierApi(object):
                                     topic=self.topic_subnet_update)
         cctxt.cast(context, 'subnet_update', subnet=subnet)
 
-    def opflex_endpoint_update(self, context, details, host=None):
-        cctxt = self.client.prepare(fanout=True,
-                                    topic=self.topic_opflex_endpoint_update,
-                                    server=host)
-        cctxt.cast(context, 'opflex_endpoint_update', details=details)
-
-    def opflex_vrf_update(self, context, details):
-        cctxt = self.client.prepare(fanout=True,
-                                    topic=self.topic_opflex_vrf_update)
-        cctxt.cast(context, 'opflex_vrf_update', details=details)
-
 
 class GBPServerRpcApiMixin(object):
-    """Agent-side RPC (stub) for agent-to-plugin interaction.
-
-    Version 1.1: add async request_* APIs
-    """
+    """Agent-side RPC (stub) for agent-to-plugin interaction."""
 
     GBP_RPC_VERSION = "1.0"
 
@@ -103,45 +80,14 @@ class GBPServerRpcApiMixin(object):
                           vrf_ids=vrf_ids, host=host)
 
     @log.log
-    def request_gbp_details(self, context, agent_id, request=None, host=None):
-        # Request is a tuple with the device_id as first element, and the
-        # request ID as second element
-        cctxt = self.client.prepare(version=self.GBP_RPC_VERSION, fanout=True)
-        cctxt.cast(context, 'request_gbp_details', agent_id=agent_id,
-                   request=request, host=host)
-
-    @log.log
-    def request_gbp_details_list(self, context, agent_id, requests=None,
-                                 host=None):
-        # Requests is a list of tuples with the device_id as first element,
-        # and the request ID as second element
-        cctxt = self.client.prepare(version=self.GBP_RPC_VERSION, fanout=True)
-        cctxt.cast(context, 'request_gbp_details_list',
-                   agent_id=agent_id, requests=requests, host=host)
-
-    @log.log
-    def request_vrf_details(self, context, agent_id, request=None, host=None):
-        # Request is a tuple with the vrf_id as first element, and the
-        # request ID as second element
-        cctxt = self.client.prepare(version=self.GBP_RPC_VERSION, fanout=True)
-        cctxt.cast(context, 'request_vrf_details', agent_id=agent_id,
-                   request=request, host=host)
-
-    @log.log
-    def request_vrf_details_list(self, context, agent_id, requests=None,
-                                 host=None):
-        # Requests is a list of tuples with the vrf_id as first element,
-        # and the request ID as second element
-        cctxt = self.client.prepare(version=self.GBP_RPC_VERSION, fanout=True)
-        cctxt.cast(context, 'request_vrf_details_list',
-                   agent_id=agent_id, requests=requests, host=host)
-
-    @log.log
     def ip_address_owner_update(self, context, agent_id, ip_owner_info,
                                 host=None):
-        cctxt = self.client.prepare(version=self.GBP_RPC_VERSION, fanout=True)
-        cctxt.cast(context, 'ip_address_owner_update', agent_id=agent_id,
-                   ip_owner_info=ip_owner_info, host=host)
+        self.fanout_cast(context,
+                         self.make_msg('ip_address_owner_update',
+                                       agent_id=agent_id,
+                                       ip_owner_info=ip_owner_info,
+                                       host=host),
+                         version=self.GBP_RPC_VERSION)
 
 
 class GBPServerRpcCallback(object):
@@ -149,14 +95,12 @@ class GBPServerRpcCallback(object):
 
     # History
     #   1.0 Initial version
-    #   1.1 Async request_* APIs
 
-    RPC_API_VERSION = "1.1"
+    RPC_API_VERSION = "1.0"
     target = oslo_messaging.Target(version=RPC_API_VERSION)
 
-    def __init__(self, gbp_driver, agent_notifier):
+    def __init__(self, gbp_driver):
         self.gbp_driver = gbp_driver
-        self.agent_notifier = agent_notifier
 
     def get_gbp_details(self, context, **kwargs):
         return self.gbp_driver.get_gbp_details(context, **kwargs)
@@ -183,46 +127,6 @@ class GBPServerRpcCallback(object):
             )
             for vrf_id in kwargs.pop('vrf_ids', [])
         ]
-
-    def request_gbp_details(self, context, **kwargs):
-        result = [self.gbp_driver.request_gbp_details(context, **kwargs)]
-        # Notify the agent back once the answer is calculated
-        self.agent_notifier.opflex_endpoint_update(context, result,
-                                                   host=kwargs.get('host'))
-
-    def request_gbp_details_list(self, context, **kwargs):
-        result = [
-            self.gbp_driver.request_gbp_details(
-                context,
-                request=request,
-                **kwargs
-            )
-            for request in kwargs.pop('requests', [])
-        ]
-
-        # Notify the agent back once the answer is calculated
-        self.agent_notifier.opflex_endpoint_update(context, result,
-                                                   host=kwargs.get('host'))
-
-    def request_vrf_details(self, context, **kwargs):
-        result = [self.gbp_driver.request_vrf_details(context, **kwargs)]
-        # Notify the agent back once the answer is calculated
-        self.agent_notifier.opflex_vrf_update(context, result,
-                                              host=kwargs.get('host'))
-
-    def request_vrf_details_list(self, context, **kwargs):
-        result = [
-            self.gbp_driver.request_vrf_details(
-                context,
-                request=request,
-                **kwargs
-            )
-            for request in kwargs.pop('requests', [])
-        ]
-
-        # Notify the agent back once the answer is calculated
-        self.agent_notifier.opflex_vrf_update(context, result,
-                                              host=kwargs.get('host'))
 
     def ip_address_owner_update(self, context, **kwargs):
         self.gbp_driver.ip_address_owner_update(context, **kwargs)
