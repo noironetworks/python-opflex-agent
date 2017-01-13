ebdbdfd95415210e11fcc7e44416c55a8fbeef7f
Ivar Lazzaro <ivarlazzaro@gmail.com>
2017-01-13 10:10:25 -0800

    Revert "Optimize get_vif_ports_by_ids"
    This reverts commit 5b60eb6733906626eec0274083a3eb89979d2f94.

--- opflexagent/utils/bridge_managers/ovs_manager.py
+++ opflexagent/utils/bridge_managers/ovs_manager.py
@@ -15,7 +15,6 @@ from neutron.common import constants as n_constants
 from neutron.i18n import _LW
 from neutron.plugins.openvswitch.common import constants
 from oslo_log import log as logging
-from oslo_serialization import jsonutils
 
 from opflexagent.utils.bridge_managers import bridge_manager_base
 
@@ -94,60 +93,3 @@ class OvsManager(bridge_manager_base.BridgeManagerBase):
         :param port: a ovs_lib.VifPort object.
         """
         pass
-
-    def get_vif_port_by_ids(self, port_ids):
-        args = ['--format=json', '--', '--columns=external_ids,name,ofport',
-                'find', 'Interface',
-                'external_ids:iface-id{<=}%s' % ','.join('"%s"' % x for
-                                                       x in port_ids)]
-        int_br = self.int_br
-        LOG.debug("_get_vif_port_by_ids args: %s", args)
-        iface_map = {}
-        results = int_br.run_vsctl(args)
-        if not results:
-            return
-        json_result = jsonutils.loads(results)
-        # Retrieve the indexes of the columns we're looking for
-        headings = json_result['headings']
-        ext_ids_idx = headings.index('external_ids')
-        name_idx = headings.index('name')
-        ofport_idx = headings.index('ofport')
-        for data in json_result['data']:
-            port_id = None
-            try:
-                # If data attribute is missing or empty the line below will
-                # raise an exeception which will be captured in this block.
-                ext_id_dict = dict((item[0], item[1]) for item in
-                                   data[ext_ids_idx][1])
-                LOG.debug('ext_id_dict: %s', ext_id_dict)
-                if 'iface-id' not in ext_id_dict:
-                    # Not an interface
-                    continue
-                port_name = data[name_idx]
-                port_id = ext_id_dict['iface-id']
-                # REVISIT(ivar): we assume iface-id won't collide
-                # (it's a UUID after all)
-                # switch = get_bridge_for_iface(self.root_helper, port_name)
-                # if switch != self.br_name:
-                #    LOG.info(_("Port: %(port_name)s is on %(switch)s,"
-                #               " not on %(br_name)s"),
-                #               {'port_name': port_name,
-                #                'switch': switch, 'br_name': self.br_name})
-                #    return
-                ofport = data[ofport_idx]
-                # ofport must be integer otherwise return None
-                if not isinstance(ofport, int) or ofport == -1:
-                    LOG.warn(_("ofport: %(ofport)s for VIF: %(vif)s is not a "
-                               "positive integer"), {'ofport': ofport,
-                                                     'vif': port_id})
-                    return
-                # Find VIF's mac address in external ids
-                vif_mac = ext_id_dict['attached-mac']
-                iface_map[port_id] = ovs_lib.VifPort(
-                    port_name, ofport, port_id, vif_mac, self)
-            except Exception as e:
-                LOG.warn(_("Unable to parse interface details. for port "
-                           "%(port)s Exception: %(exc)s"), {'port': port_id,
-                                                            'exc': e})
-                continue
-        return iface_map
