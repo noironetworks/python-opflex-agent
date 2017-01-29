405727eb8df6c44b1b4f0a04fc1f6cf62158c858
Amit Bose <amitbose@gmail.com>
2017-01-28 21:53:25 -0800

    Bump version to 4.0.2
    Signed-off-by: Amit Bose <amitbose@gmail.com>

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ import setuptools
 
 setuptools.setup(
     name="neutron-opflex-agent",
-    version="4.0.1",
+    version="4.0.2",
     packages=setuptools.find_packages(exclude=["*.tests", "*.tests.*",
                                                "tests.*", "tests"]),
     author="Cisco Systems, Inc.",
