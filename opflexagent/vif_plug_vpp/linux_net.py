2d5664d9ede2bf1ae1684e0bbc6048decc3b092f
Kiran Shastri <shastrinator@gmail.com>
2018-05-30 15:50:47 -0700

    VPP agent changes: 1. VPP dhcp driver and host-interface changes 2. Restartability for gbp-vpp-agent 3. More interface settings: state/mtu 4. Adopting vpplib in opflexagent path 5. vpplib connection fixes and cleanup
    (cherry picked from commit 3a7f0cfcdf45b96ce57b7d0766190b84577dd068)

--- opflexagent/vif_plug_vpp/linux_net.py
+++ opflexagent/vif_plug_vpp/linux_net.py
@@ -15,84 +15,58 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-"""Implements vlans, bridges using linux utilities."""
-import sys
+"""Creates VPP ports so that VPP renderer can add it to the right bridge."""
 
+import opflexagent.vpplib.VPPApi as vpp_api
+from opflexagent.vpplib.VPPApi import VPPApi
 from oslo_log import log as logging
-
 from vif_plug_vpp import constants
-from vpplib.VPPApi import VPPApi
 
 LOG = logging.getLogger(__name__)
 
 
-def mac_to_bytes(mac):
-    return str(''.join(chr(int(x, base=16)) for x in mac.split(':')))
-
-
-def _create_vpp_vif(dev, iface_id, mac,
+def _create_vpp_vif(dev, iface_id, mac, mtu,
                     instance_id, interface_type=None,
                     vhost_server_path=None):
     vapi = VPPApi(LOG, 'nova_os_vif')
-    mac_address = mac_to_bytes(mac)
+    # iface_id is the port UUID as seen in neutron. Use this as the tag
+    # on the interface which will be later used as the key for endpoint lookup
+    mac_address = vpp_api.mac_to_bytes(mac)
     if interface_type == constants.VPP_VHOSTUSER_CLIENT_INTERFACE_TYPE:
-        vapi.create_vhost_user_if(str(vhost_server_path).encode('utf-8'))
+        sw_if_index = vapi.create_vhost_user_if(
+                        str(vhost_server_path).encode('utf-8'),
+                        0, mac_address, iface_id)
     else:
-        vapi.create_vhost_user_if(str(vhost_server_path).encode('utf-8'),
-                                  1, mac_address)
-    return 0
+        sw_if_index = vapi.create_vhost_user_if(
+                        str(vhost_server_path).encode('utf-8'),
+                        1, mac_address, iface_id)
+    LOG.debug("sw_if_index:{}".format(sw_if_index))
+    if mtu:
+        vapi.set_interface_mtu(sw_if_index, mtu)
+    # Default admin state  for port in VPP is down, so do admin-up here
+    vapi.set_interface_state(sw_if_index, 1)
+    return sw_if_index
 
 
 def create_vpp_vif_port(dev, iface_id, mac, instance_id,
                         mtu=None, interface_type=None, timeout=None,
                         vhost_server_path=None):
-    _create_vpp_vif(dev, iface_id, mac, instance_id,
+    sw_if_index = _create_vpp_vif(dev, iface_id, mac, mtu, instance_id,
                     interface_type,
                     vhost_server_path)
-    _update_device_mtu(dev, mtu, interface_type, timeout=timeout)
+    return sw_if_index
 
 
 def update_vpp_vif_port(dev, mtu=None, interface_type=None, timeout=None):
-    _update_device_mtu(dev, mtu, interface_type, timeout=timeout)
+    if not mtu:
+        return
+    vapi = VPPApi(LOG, 'nova_os_vif')
+    _, _, sw_if_index = vapi.vhost_details_from_tag(
+        dev.port_profile.interface_id)
+    vapi.set_interface_mtu(sw_if_index, mtu)
 
 
 def delete_vpp_vif_port(dev, timeout=None, delete_netdev=True):
     vapi = VPPApi(LOG, 'nova_os_vif')
     vapi.delete_vhost_user_if(dev)
     return 0
-
-
-# TBD
-def _set_device_mtu(dev, mtu):
-    pass
-
-
-def _update_device_mtu(dev, mtu, interface_type=None, timeout=120):
-    if not mtu:
-        return
-    if interface_type not in [
-        constants.VPP_VHOSTUSER_INTERFACE_TYPE,
-        constants.VPP_VHOSTUSER_CLIENT_INTERFACE_TYPE]:
-        if sys.platform != constants.PLATFORM_WIN32:
-            # Hyper-V with OVS does not support external programming of virtual
-            # interface MTUs via netsh or other Windows tools.
-            # When plugging an interface on Windows, we therefore skip
-            # programming the MTU and fallback to DHCP advertisement.
-            _set_device_mtu(dev, mtu)
-    elif _vpp_supports_mtu_requests(timeout=timeout):
-        _set_mtu_request(dev, mtu, timeout=timeout)
-    else:
-        LOG.debug("MTU not set on %(interface_name)s interface "
-                  "of type %(interface_type)s.",
-                  {'interface_name': dev,
-                   'interface_type': interface_type})
-
-
-# TBD: How does VPP set mtu?
-def _set_mtu_request(dev, mtu, timeout=None):
-    pass
-
-
-# TBD: How does VPP set mtu?
-def _vpp_supports_mtu_requests(timeout=None):
-    return False
