20a49a637027bff7a117d77872babb432104f921
Thomas Bachman <tbachman@yahoo.com>
2018-10-05 16:18:06 +0000

    Bump version to 5.6.1

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ import setuptools
 
 setuptools.setup(
     name="neutron-opflex-agent",
-    version="5.6.0",
+    version="5.6.1",
     packages=setuptools.find_packages(exclude=["*.tests", "*.tests.*",
                                                "tests.*", "tests"]),
     author="Cisco Systems, Inc.",
