83e1c96f8f3f4cd6b858db712f1da89d9cc50335
Thomas Bachman <tbachman@yahoo.com>
2018-10-26 15:28:59 +0000

    Bump version to 7.3.4

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ import setuptools
 
 setuptools.setup(
     name="neutron-opflex-agent",
-    version="7.3.3",
+    version="7.3.4",
     packages=setuptools.find_packages(exclude=["*.tests", "*.tests.*",
                                                "tests.*", "tests"]),
     author="Cisco Systems, Inc.",
