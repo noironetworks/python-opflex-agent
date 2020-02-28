040e5c442ba8d7c14ac16ad204410fbecc367965
Thomas Bachman <tbachman@yahoo.com>
2020-02-28 14:24:06 +0000

    Bump version to 5.7.5

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ import setuptools
 
 setuptools.setup(
     name="neutron-opflex-agent",
-    version="5.7.4",
+    version="5.7.5",
     packages=setuptools.find_packages(exclude=["*.tests", "*.tests.*",
                                                "tests.*", "tests"]),
     author="Cisco Systems, Inc.",
