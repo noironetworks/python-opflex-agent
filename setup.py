710ee3c4c1e762b8e3f4358ed0c4b2b226c1c598
Thomas Bachman <tbachman@yahoo.com>
2020-09-08 17:55:42 +0000

    Bump version to 11.0.0

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ import setuptools
 
 setuptools.setup(
     name="neutron-opflex-agent",
-    version="7.0.0",
+    version="11.0.0",
     packages=setuptools.find_packages(exclude=["*.tests", "*.tests.*",
                                                "tests.*", "tests"]),
     author="Cisco Systems, Inc.",
