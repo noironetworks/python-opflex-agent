ccfe2db9563f840831af9dd8cea4a4763f488859
Thomas Bachman <tbachman@yahoo.com>
2022-03-09 02:54:04 +0000

    Bump version for victoria stable branch

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ import setuptools
 
 setuptools.setup(
     name="neutron-opflex-agent",
-    version="13.1.0",
+    version="13.1.1",
     packages=setuptools.find_packages(exclude=["*.tests", "*.tests.*",
                                                "tests.*", "tests"]),
     author="Cisco Systems, Inc.",
