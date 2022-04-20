e1a5d06b0ff220e03e54354c94b08e7f3204d99f
Thomas Bachman <tbachman@yahoo.com>
2022-04-20 13:54:39 +0000

    Bump version for train stable branch

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ import setuptools
 
 setuptools.setup(
     name="neutron-opflex-agent",
-    version="11.5.1",
+    version="11.6.0",
     packages=setuptools.find_packages(exclude=["*.tests", "*.tests.*",
                                                "tests.*", "tests"]),
     author="Cisco Systems, Inc.",
