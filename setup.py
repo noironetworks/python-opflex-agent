0ff3f07d2d03bdcd702d4492e9031604f03f0554
Thomas Bachman <tbachman@yahoo.com>
2022-09-06 12:18:44 +0000

    Bump version for xena stable branch

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ import setuptools
 
 setuptools.setup(
     name="neutron-opflex-agent",
-    version="15.1.0",
+    version="15.2.0",
     packages=setuptools.find_packages(exclude=["*.tests", "*.tests.*",
                                                "tests.*", "tests"]),
     author="Cisco Systems, Inc.",
