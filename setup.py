2db799a5935541a0977f47bcc49a1557d5e164e9
Thomas Bachman <tbachman@yahoo.com>
2022-10-12 02:21:11 +0000

    Bump version for ussuri stable branch

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ import setuptools
 
 setuptools.setup(
     name="neutron-opflex-agent",
-    version="12.4.1",
+    version="12.4.2",
     packages=setuptools.find_packages(exclude=["*.tests", "*.tests.*",
                                                "tests.*", "tests"]),
     author="Cisco Systems, Inc.",
